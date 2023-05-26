16e10ac73afde4b0803ff43ddfd49c702eb915d2
Sumukha87 <73688740+Sumukha87@users.noreply.github.com>
2023-05-27 00:45:03 +0530

    Add files via upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {logo} from "../assets"
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+            <img src={logo} className='w-28 object-contain'/>
+            <button type='button' onClick={()=>window.open('https://github.com/Sumukha87')} className='black_btn'>
+                GitHub Page
+
+            </button>
+
+        </nav>
+        <h1 className='head_text'>
+            Summarize all of your Articals with<br className='max-md:hidden'/>
+            <span>Open AI APIS</span>
+        </h1>
+        <h2 className='desc'>
+            Get the summery of any article of your choice with an open source summarier with uses Chat GPT API to get a concise summery.
+
+        </h2>
+
+    </header>
+  )
+}
+
+export default Hero
